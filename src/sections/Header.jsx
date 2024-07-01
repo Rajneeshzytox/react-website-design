ed7b71a5b5355f4e09b725fc131a79f231d4f988
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-01 15:38:43 +0530

    header section created

---
+++ src/sections/Header.jsx
@@ -0,0 +1,18 @@
+import logo from  "../assets/logo.png"
+
+export default function Header(){
+    return(
+        <header className="max-w-screen-lg flex justify-between items-center px-5 py-3 mx-auto my-10 max-h-14">
+
+            <div className="logo-containerb bg-slate-200 w-1/3">
+                <img src={logo} className="max-w-full"/>
+            </div>
+
+            <div className="w-1/3 min-w-56 flex justify-center items-cente ring">
+                <input type="search" placeholder="search" className="px-2 py-2 w-full bg-slate-50 outline-none"/>
+
+                <button className=" bg-blue-500 px-2 py-1 flex justify-center items-center "><span className="material-symbols-outlined text-white">search</span></button>
+            </div>
+        </header>
+    )
+}
---
+++ src/sections/Page.jsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <section className="max-w-[100%] md:max-w-[90%] lg:max-w-[80%] bg-slate-50 mx-auto min-h-[100vh] rounded-md">
+
+        </section>
+    )
+}
